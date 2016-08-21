1662298cae4cdf3ac32ae1ef9aabcfe96e27214e
İstemi Dülger <istemidulger@gmail.com>
2016-08-21 20:28:18 +0300

    Add files via upload

---
+++ fallout_wl.js
@@ -0,0 +1,134 @@
+/* Fallout Weapons */
+//my house rules doesn't account for Energy Weapons for their usage, only to maintain them so they are all mixed in
+/* Handmade Guns */
+var handmade = ["Pipe Pistol - Break Action 9mm",
+"Smith & Wesson .32 pistol - .32",
+"Derringer - .32",
+"Pipe Rifle - Break Action 9mm",
+"Pipe Rifle - Break Action 10mm",
+"Pipe Rifle - Bolt Action 10mm",
+"Shotgonne - Breach Load .44"];
+/* Pre-historic crap */
+var lamepistols = ["Browning Hi-Power - 9mm",
+"Barretta 92fs - 9mm",
+"Shanxi Type 17 -10mm", 
+"Colt 6520 - 10mm",
+"Colt M1911 .45 - .45",
+"Casull Revolver - .45",
+"Desert Eagle .44 - .44",
+"Smith & Wesson Model 29 - .44",
+"Glock 20 - 10mm",
+"Wattz 500 Laser Pistol - Small Energy Cell",
+"Spasm NeuroDisruptor - Small Energy Cell"];
+
+var pistols = ["Sig-Sauer Auto Pistol - 14mm",
+"That Gun - 5.56mm",
+"Colt Python - .357",
+"Mateba Autorevolver - .357",
+"Desert Eagle .50 - .50 AE",
+"Colt Delta Elite - 10mm",
+"Zhu-Rong v418 - 10mm",
+"Wattz 1000 Laser Pistol - Small Energy Cell",
+"Glock 80 Plasma Pistol - Small Energy Cell",
+"Glock 86 Plasma Defender - Small Energy Cell",
+"YK32 Pulse Pistol - Small Energy Cell"];
+
+
+var lamesmgs = ["MP5 - 9mm",
+"MP9 - 10mm",
+"Tommy Gun - .45",
+"Walther MPL - 9mm",
+"Mini Uzi - 9mm",
+"Uzi - 10mm",
+"P90 - 5.56mm",
+"Grease Gun - .45"];
+
+var smgs = ["",
+""]
+
+var lamerifle = ["Varmint rifle - 5.56mm",
+"Hunting rifle - 7.62mm",
+"Hunting rifle - .50",
+"Ak-112 - 5.56mm",
+"Service Rifle - 5.56mm",
+"Lever Action - .357"];
+var lamemelee = ["Shiv - Rusted",
+"Knife - Utensil",
+"Knife - Butcher",
+"4x4 Board",
+"4x4 Board - Nail1",
+"Trench Shovel -Rusted",
+"Pipe - Water",
+"Baseball bat - Wooden",
+"Baseball bat - Aliminum",
+"Pole - Wooden",
+"Pole - Metal",
+"Shiv - Broken Edge"];
+var lameexplosives = ["Firecrackers",
+"Box Of Firecrackers",
+"Fireworks",
+"Fireworks Bundle",
+"Molotov Cocktail - Half Empty",
+"Molotov Cocktail - Dried",
+"Dynamite Stick",
+"Homemade Fertilizer Grenade",
+"Homemade Fertilizer - Satchel"];
+
+
+
+/* Weapon Conditions */
+var firearmcondition = ["Jammed - Rusted",
+"Jammed - Misfed, Deformed Loading Mechanism",
+"Jammed - Obstructed By Debris",
+"Rusted - Beyond Repair",
+"Rusted - Salvage",
+"Rusted - Fixable",
+"Missing Part - Trigger",
+"Missing Part - Shattered Muzzle",
+"Missing Part - Striker",
+"Missing Part - Broken in half",
+"Missing Part - Furniture & Body",
+"Missing Part - Loading Spring",
+"Debris - Melted",
+"Debris - Radioactive",
+"Servicable - Well Maintained",
+"Servicable - Sealed In Plastic Bag",
+"Servicable - Recently Fired",
+"Servicable - Freshly Oiled",
+"Good as new",
+"Mint Condition",
+"Mint Condition - All Accessories",
+"Mint Condition - Still in its case"];
+
+var explosiveconditions = ["Bad Fuse",
+"Corroded",
+"Wet",
+"Missing - Half",
+"Missing - Broken Casing",
+"Single",
+"Ready to go",
+"Mint",
+"In Military Case",
+"Armed",
+"Ingredients - Missing Fuse",
+"Ingredients - Including Recipe",
+"Ingredients - Just Chemical Jars",
+"Ingredients - Missing Casing",
+"Military Crate"];
+
+var meleeconditions = ["Rusted",
+"Barely Together",
+"Dull",
+"Debris - Melted",
+"Debris - Radioactive",
+"Missing Part - Broken Handle",
+"Missing Part - Broken In Half",
+"Bent",
+"Servicable",
+"Dirty - Toxic Mud",
+"Dirty - Radioactive Goo",
+"Good Shape",
+"Well Worn",
+"Sharpened - Reinforced",
+"Sturdy",
+"Antiquated"];
